b5e3c0c2d01fd4b8b9ed706ab8eaa3de793c5b4a
Yury Filionchyk <yury.filionchyk@d-tech.com.pl>
2025-06-06 14:45:52 +0200

    2311 -->8080

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var appUrl = Environment.GetEnvironmentVariable("APP_URL")+":2311";
+var appUrl = Environment.GetEnvironmentVariable("APP_URL");// +":2311";
 if (!string.IsNullOrEmpty(appUrl) && !appUrl.StartsWith("http"))
     appUrl = "https://" + appUrl;
 if (!string.IsNullOrEmpty(appUrl))
